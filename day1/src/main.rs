92c67ae12d53eb04f4874d92718cbbc270b883e8
Eetu Suonpää <eetu.suonpaa@solita.fi>
2023-12-01 19:43:24 +0200

    comment what to do

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+    // Käy läpi jokainen rivi, kato onko numeroita
+    // ja nappaa eka ja vika (eka==vika jos vaan yks)
+    // Sit muodosta niistä kahden numeron luku ja laske
+    // kaikkien lukujen summa
 }
